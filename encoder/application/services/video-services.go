12dec3fc8c3c6d3c4eccc7f0ccfc82d68c341a31
Renato Spakauskas <renatospaka@gmail.com>
2021-01-12 17:14:53 -0300

    feat: :construction: Video Services

---
+++ encoder/application/services/video-services.go
@@ -0,0 +1,61 @@
+package services
+
+import (
+	"context"
+	"encoder/application/repositories"
+	"encoder/domain"
+	"io/ioutil"
+	"log"
+	"os"
+
+	"cloud.google.com/go/storage"
+)
+
+type VideoService struct {
+	Video           *domain.Video
+	VideoRepository repositories.VideoRepository
+}
+
+func NewVideoService() VideoService {
+	return VideoService{}
+}
+
+func (v *VideoService) Download(bucketName string) error {
+	ctx := context.Background()
+	client, err := storage.NewClient(ctx)
+	if err != nil {
+		return err
+	}
+
+	bkt := client.Bucket(bucketName)
+	obj := bkt.Object(v.Video.FilePath)
+	
+	r, err := obj.NewReader(ctx)
+	if err != nil {
+		return err
+	}
+	defer r.Close()
+
+	body, err := ioutil.ReadAll(r)
+	if err != nil {
+		return err
+	}
+
+	file, err := os.Create(os.Getenv("localStoragePath") + "/" + v.Video.ID + ".mp4")
+	if err != nil {
+		return err
+	}
+
+	// test if it worked
+	_, err = file.Write(body)
+	if err != nil {
+		return err
+	}
+
+	defer file.Close()
+
+	log.Printf("The video %v has been stored.", v.Video.ID)
+
+	//no errors ocurred
+	return nil
+}
--- encoder/go.mod
+++ encoder/go.mod
@@ -3,11 +3,12 @@ module encoder
 go 1.14
 
 require (
+	cloud.google.com/go/storage v1.12.0
 	github.com/asaskevich/govalidator v0.0.0-20200907205600-7a23bdc65eef
 	github.com/jinzhu/gorm v1.9.16
 	github.com/lib/pq v1.1.1
 	github.com/satori/go.uuid v1.2.0
 	github.com/sirupsen/logrus v1.7.0
-	github.com/stretchr/testify v1.2.2
+	github.com/stretchr/testify v1.4.0
 	gopkg.in/check.v1 v1.0.0-20201130134442-10cb98267c6c // indirect
 )
