3af01a2c696b2e666f30e48efce5864923a5f4a9
Renato Spakauskas <renatospaka@gmail.com>
2021-01-12 16:08:47 -0300

    test: :white_check_mark: Video and Jobs repo tests passed

--- encoder/application/repositories/job_repository_test.go
+++ encoder/application/repositories/job_repository_test.go
@@ -38,3 +38,34 @@ func TestJobRepositoryDbInsert(t *testing.T) {
 	require.Equal(t, j.ID, job.ID)
 	require.Equal(t, j.VideoID, video.ID)
 }
+
+func TestJobRepositoryDbUpdate(t *testing.T) {
+	db := database.NewDbTest()
+	defer db.Close()
+	
+	video := domain.NewVideo()
+	video.ID = uuid.NewV4().String()
+	video.FilePath = "path"
+	video.CreatedAt = time.Now()
+
+	// insert the video into the repository
+	repo := repositories.VideoRepositoryDb{Db: db}
+	repo.Insert(video)
+
+	job, err := domain.NewJob("output_path", "Pending", video)
+	require.Nil(t, err)
+	
+	// insert the job into the repository
+	repoJob := repositories.JobRepositoryDb{Db:db}
+	repoJob.Insert(job)
+
+	// force an update in the status
+	job.Status = "Complete"
+	repoJob.Update(job)
+
+	//retrieve the job just updated above
+	j, err := repoJob.Find(job.ID)
+	require.NotEmpty(t, j.ID)
+	require.Nil(t, err)
+	require.Equal(t, j.Status, job.Status)
+}
